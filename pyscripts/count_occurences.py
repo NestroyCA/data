d199141d4efaeb72d71d43f491c36f8be4223b4f
cfhaak <c.f.haak@posteo.de>
2023-09-28 14:59:26 +0200

    basic counter function, upload not working, see https://github.com/NestroyCA/baserow-entities/issues/2

--- pyscripts/count_occurences.py
+++ pyscripts/count_occurences.py
@@ -1,10 +1,57 @@
-import os
+import requests
+import glob
+from config import br_client, BASEROW_DB_ID, JSON_FOLDER
+from acdh_tei_pyutils.tei import TeiReader
 import json
-from data.python_scripts.config import br_client, BASEROW_DB_ID, JSON_FOLDER
 
-os.makedirs(JSON_FOLDER, exist_ok=True)
-json_file_paths = br_client.dump_tables_as_json(
-    BASEROW_DB_ID,
-    folder_name="json_dumps", 
-    indent=2
+editions_path="./xmls/"
+places_xpath=".//tei:rs[@type='place']/@target"
+places_prefix = "place:"
+places_table_id = "2563"
+place_id_field = "field_23582"
+total_occurences_field = "field_23759"
+baserow_nestroy_id_key = "nestroy_id"
+baserow_counter_key = "total_occurences"
+
+
+
+def count_place_occurences():
+    place_id_2_occ_counter = {}
+    for file in glob.glob(editions_path):
+        doc = TeiReader(file)
+        for place_ref in doc.any_xpath(places_xpath):
+            place_id = place_ref.removeprefix(places_prefix)
+            if place_id not in place_id_2_occ_counter:
+                place_id_2_occ_counter[place_id] = 0
+            place_id_2_occ_counter[place_id] += 1
+    return place_id_2_occ_counter
+
+
+def update_item_online(item:dict):
+    update_target_url = f"{br_client.br_base_url}database/rows/table/{places_table_id}/{item['id']}/?user_field_names=true"
+    result = requests.patch(
+        update_target_url,
+        headers={
+            "Authorization": f"Token {br_client.br_token}",
+            "Content-Type": "application/json",
+        },
+        json=json.dumps(item)
+    )
+    return result
+
+
+ids_2_places = dict(
+    (place[baserow_nestroy_id_key], place) for place in  br_client.yield_rows(places_table_id)
 )
+
+place_ids_2_count = count_place_occurences()
+
+for place_id, item in ids_2_places.items():
+    if place_id not in place_ids_2_count:
+        place_ids_2_count[place_id] = 0
+    count = place_ids_2_count[place_id]
+    item[baserow_counter_key] = count
+    print(
+        update_item_online(item)
+    )
+    
