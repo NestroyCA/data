ad2d2baf948feab32a1c68dd38b1838baa96fb86
cfhaak <c.f.haak@posteo.de>
2023-09-28 12:25:39 +0200

    basics to count occurences of places

---
+++ count_occurences.py
@@ -0,0 +1,10 @@
+import os
+import json
+from config import br_client, BASEROW_DB_ID, JSON_FOLDER
+
+os.makedirs(JSON_FOLDER, exist_ok=True)
+json_file_paths = br_client.dump_tables_as_json(
+    BASEROW_DB_ID,
+    folder_name="json_dumps", 
+    indent=2
+)
